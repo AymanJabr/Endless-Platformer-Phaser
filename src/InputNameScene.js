8f010e4dccebc254288f02ab2bd8c2a98b0debb8
ayman <aymanjaber2012@gmail.com>
2021-05-19 22:03:17 +0300

    Add Input Name scene

---
+++ src/InputNameScene.js
@@ -0,0 +1,18 @@
+import 'phaser';
+/* eslint-disable */
+export default class InputNameScene extends Phaser.Scene {
+    constructor() {
+        super('inputNameScene');
+    }
+
+    init(data) {
+        // this.score = data.score;
+        // this.data = data.data.join(', ');
+    }
+
+
+    create() {
+        
+    }
+
+}
