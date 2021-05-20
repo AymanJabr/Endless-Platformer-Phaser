6f09e6413afc119af4e5a1f2dca69922faec5a56
ayman <aymanjaber2012@gmail.com>
2021-05-20 08:43:53 +0300

    Create new game

--- game-creation.js
+++ game-creation.js
@@ -1,19 +1,21 @@
 const fetch = require('node-fetch');
 
-// 'Game with ID: efrnqEJRF6YCD6jcqp0F added.'
+// 'Game with ID: FtTbSPgjuEfSKeMCYm4O  added.'
 
-fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
-  method: 'POST',
-  mode: 'cors',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-    name: 'Bomb Doger',
-  }),
-})
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((err) => {
-    console.error(err);
+
+async function createGame() {
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: 'Bomb Doger',
+    }),
   });
+  const data = await response.json();
+  console.log(data)
+}
+
+createGame();
